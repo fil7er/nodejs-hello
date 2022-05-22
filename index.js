392ad5fb74df604826bc654296ee9804248fe5f2
fil7er <fil7er@gmail.com>
2022-05-22 12:14:37 -0400

    Update index.js
    Setting PORT var

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 const process = require('process');
-
+const PORT = process.env.PORT || 5000;
 var expressapp = express();
-expressapp.get('/', function (req, res) {
+expressapp.get(PORT, function (req, res) {
     res.send('{"message":"Hello World by fil7er v3"}')
 });
-expressapp.listen(5000, function () {
+expressapp.listen(PORT, function () {
     console.log('for testing purposes only!')
 });
