2b7963c336a822a2da5f481a46af3a48b8ba0973
fil7er <fil7er@gmail.com>
2022-05-22 11:59:07 -0400

    bugfix

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var express = require('express')
 const process = require('process');
 
-var expressapp = express()
+var expressapp = express();
 expressapp.get('/', function (req, res) {
     res.send('{"message":"Hello World by fil7er v3"}')
-})
+});
 expressapp.listen(5000, function () {
     console.log('for testing purposes only!')
-})
-
-/* To Handle Ctrl C*/
-process.on('SIGINT', function () {
-    process.exit();
 });
